aa5f2a4c711a4afe87ad78d6f221c5bb6144fbf5
Faisal Kanout <fkanout@mbp-de-axway.wks.ptx.axway.int>
2018-01-02 16:22:42 +0100

    FIX - App bin.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import { updateDeliveryManPosition, getAvailbleDeliveryMen } from './services/gps';
 
-
-(async () => {
+const app = async () => {
+  console.log('started');
+  console.log(process.env.ENV);
   console.log(await updateDeliveryManPosition('700', 'geogeo'));
   // console.log(await getAvailbleDeliveryMen());
-})();
+};
 
+export default app;
