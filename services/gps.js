4921bfcf49ab71ae6b8aaa54f3c791a9be9b6090
Faisal Kanout <fkanout@mbp-de-axway.wks.ptx.axway.int>
2018-01-02 15:46:57 +0100

    eslint

--- services/gps.js
+++ services/gps.js
@@ -3,5 +3,9 @@ import { saveHashKeyValue, getAllDocHashes } from './storage';
 
 const deliveryMenDoc = process.env.DELIVERY_MEN_DOC || 'deliverymen';
 
-export const updateDeliveryManPosition = async (deliveryManId, gps) => saveHashKeyValue(deliveryMenDoc, deliveryManId, gps);
-export const getAvailbleDeliveryMen = async () => getAllDocHashes(deliveryMenDoc);
+export const updateDeliveryManPosition =
+    async (deliveryManId, gps) => saveHashKeyValue(deliveryMenDoc, deliveryManId, gps);
+
+export const getAvailbleDeliveryMen =
+    async () => getAllDocHashes(deliveryMenDoc);
+
